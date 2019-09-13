992a0d451a73d4179396f4111a08f92efa6412f1
”Mina” <minaa.hosseinii@gmail.com>
2019-09-13 16:22:54 +0200

    Input and Fireman positions added

---
+++ Assets/_Game/Scripts/Buttoninput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttoninput : MonoBehaviour
+{
+    public bool left;
+    private void OnMouseDown()
+    {
+
+        if (left)
+        {
+            Debug.Log("touch!");
+        }
+        else
+        {
+            Debug.Log("touch right!");
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/FiremanController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiremanController : MonoBehaviour
+{
+   public List<Transform> positions = new List<Transform>();
+
+    private void Start()
+    {
+        transform.position = positions[1].position;
+    }
+}
